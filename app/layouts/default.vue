96bc63d83f755908b922d02debb5872c94637f1d
Aaron Dewes <aaron@nirvati.org>
2025-07-14 11:05:07 +0200

    Fix typo

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,7 +2,7 @@
     <div class="h-dvh w-dvw font-excalifont">
         <header class="flex items-center">
             <img src="~/assets/plfanzen.svg" alt="Plfanzen Logo" class="h-24 w-24 mr-4">
-            <h1 class="text-4xl font-bold text-gray-800">CTF</h1>
+            <h1 class="text-4xl font-bold text-gray-800">CFT</h1>
             <div class="ml-auto flex items-center space-x-4 mr-4 text-lg">
                 <NuxtLink to="/" class="text-gray-600 hover:text-gray-800">Heraufsorderungen</NuxtLink>
                 <NuxtLink to="/scoreboard" class="text-gray-600 hover:text-gray-800">Punntebertt</NuxtLink>
